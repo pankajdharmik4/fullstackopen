95e9878d67a55aeb90d59fbf4bf25ca789050061
Pankaj Dharmik <pankajdharmik1999@gmail.com>
2023-09-17 20:51:10 +0530

    1.6: unicafe step1 Feedback Collection

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const Button = ({text, handleClick})=>{
+  return(
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handleGood = ()=>{
+    setGood(good+1)
+  }
+
+  const handleNeutral = ()=>{
+    setNeutral(neutral+1)
+  }
+
+  const handleBad = ()=>{
+    setBad(bad+1)
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={handleGood}/>
+      <Button text="neutral" handleClick={handleNeutral}/>
+      <Button text="bad" handleClick={handleBad}/>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
