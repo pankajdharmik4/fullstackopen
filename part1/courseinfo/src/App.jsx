9db775d7c184f331c8ee7ffc5825955daf28e879
Pankaj Dharmik <pankajdharmik1999@gmail.com>
2023-09-17 13:20:41 +0530

    1.3: course information step3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,16 +1,17 @@
 
 const Header = (props)=>{
+  console.log(props)
   return(
-    <h1>{props.course.name}</h1>
+    <h1>{props.course}</h1>
   )
 }
 
 const Content = (props)=>{
   return(
     <>
-    <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-    <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-    <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    <Part part={props.part1.name} exercises={props.part1.exercises}/>
+    <Part part={props.part2.name} exercises={props.part2.exercises}/>
+    <Part part={props.part3.name} exercises={props.part3.exercises}/>
     </>
   )
 }
@@ -23,34 +24,30 @@ const Part = (props) =>{
 
 const Total = (props)=>{
   return(
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
   }
 
   return (
     <div>
       <Header course = {course}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total part1={part1} part2={part2} part3={part3}/>
     </div>
   )
 }
