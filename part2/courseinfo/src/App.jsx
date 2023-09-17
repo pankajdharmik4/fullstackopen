c7d602904fc62ecb8d28a731ec97ae5b51a73890
Pankaj Dharmik <pankajdharmik1999@gmail.com>
2023-09-18 01:02:28 +0530

    2.5: separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,43 +1,5 @@
 
-const Header = (props)=>{
-  console.log(props)
-  return(
-    <h2>{props.course.name}</h2>
-  )
-}
-
-const Content = ({parts})=>{
-  console.log(parts)
-  return(
-    <>
-    {parts.map((part) => <Part key={part.id} part={part}/>)}
-    </>
-  )
-}
-
-const Part = ({part}) =>{
-  return(
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Total = ({parts})=>{
-  const sumx = parts.reduce((sum,part)=> sum+ part.exercises,0 )
-  return(
-    <>
-      <strong>Total of {sumx} exercises</strong>
-    </>
-  )
-}
-
-const Course = ({course})=>{
-  return(
-  <>
-      <Header course = {course}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-  </>)
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
