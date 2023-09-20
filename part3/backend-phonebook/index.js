82485b020a64cecbf509d2df3de35355013c2cae
Pankaj Dharmik <pankajdharmik1999@gmail.com>
2023-09-20 23:18:40 +0530

    3.16: Error Handling

--- part3/backend-phonebook/index.js
+++ part3/backend-phonebook/index.js
@@ -63,6 +63,17 @@ app.post('/api/persons',(req,res)=>{
     })
 })
 
+const errorHandler = (error, request, response, next)=>{
+    console.error(error.message)
+
+    if(error.name = 'CastError'){
+        return response.status(400).send({error: 'malformatted id'})
+    }
+    next(error);
+}
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT | 3001
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT: ${PORT}`)
